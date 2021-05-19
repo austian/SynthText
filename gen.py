999a5751190af3944a25eab129835b4031983d32
Alex Ustian <alex.l.ustian@gmail.com>
2021-05-19 14:12:51 -0400

    per character segmentation

--- gen.py
+++ gen.py
@@ -19,19 +19,24 @@ import os.path as osp
 from synthgen import *
 from common import *
 import wget, tarfile
+import pickle as cp
+import skimage.io
+from PIL import Image
 
 
 ## Define some configuration variables:
-NUM_IMG = -1 # no. of images to use for generation (-1 to use all available):
+NUM_IMG = 1000#-1 # no. of images to use for generation (-1 to use all available):
 INSTANCE_PER_IMAGE = 1 # no. of times to use the same image
-SECS_PER_IMG = 5 #max time per image in seconds
+SECS_PER_IMG = None #max time per image in seconds
 
 # path to the data-file, containing image, depth and segmentation:
 DATA_PATH = 'data'
 DB_FNAME = osp.join(DATA_PATH,'dset.h5')
 # url of the data (google-drive public file):
 DATA_URL = 'http://www.robots.ox.ac.uk/~ankush/data.tar.gz'
-OUT_FILE = 'results/SynthText.h5'
+OUT_FILE = '/Users/alexustian/synth_text_results/SynthTextFull.h5'
+
+PREPROC_DIR = "/Users/alexustian/preprocessed_images"
 
 def get_data():
   """
@@ -41,7 +46,6 @@ def get_data():
   if not osp.exists(DB_FNAME):
     try:
       colorprint(Color.BLUE,'\tdownloading data (56 M) from: '+DATA_URL,bold=True)
-      print
       sys.stdout.flush()
       out_fname = 'data.tar.gz'
       wget.download(DATA_URL,out=out_fname)
@@ -52,7 +56,7 @@ def get_data():
       colorprint(Color.BLUE,'\n\tdata saved at:'+DB_FNAME,bold=True)
       sys.stdout.flush()
     except:
-      print colorize(Color.RED,'Data not found and have problems downloading.',bold=True)
+      print(colorize(Color.RED,'Data not found and have problems downloading.',bold=True))
       sys.stdout.flush()
       sys.exit(-1)
   # open the h5 file and return:
@@ -65,9 +69,10 @@ def add_res_to_db(imgname,res,db):
   and other metadata to the dataset.
   """
   ninstance = len(res)
-  for i in xrange(ninstance):
+  for i in range(ninstance):
     dname = "%s_%d"%(imgname, i)
     db['data'].create_dataset(dname,data=res[i]['img'])
+    db['data'].create_dataset(dname+"_mask", data=res[i]['mask'])
     db['data'][dname].attrs['charBB'] = res[i]['charBB']
     db['data'][dname].attrs['wordBB'] = res[i]['wordBB']        
     db['data'][dname].attrs['txt'] = res[i]['txt']
@@ -75,46 +80,56 @@ def add_res_to_db(imgname,res,db):
 
 def main(viz=False):
   # open databases:
-  print colorize(Color.BLUE,'getting data..',bold=True)
-  db = get_data()
-  print colorize(Color.BLUE,'\t-> done',bold=True)
+#  print(colorize(Color.BLUE,'getting data..',bold=True))
+#  db = get_data()
+#  print(colorize(Color.BLUE,'\t-> done',bold=True))
 
   # open the output h5 file:
   out_db = h5py.File(OUT_FILE,'w')
   out_db.create_group('/data')
-  print colorize(Color.GREEN,'Storing the output in: '+OUT_FILE, bold=True)
+  print(colorize(Color.GREEN,'Storing the output in: '+OUT_FILE, bold=True))
+
+  im_dir = osp.join(PREPROC_DIR, 'bg_img')
+  depth_db = h5py.File(osp.join(PREPROC_DIR, 'depth.h5'),'r')
+  seg_db = h5py.File(osp.join(PREPROC_DIR, 'seg.h5'),'r')
+
+  imnames = sorted(depth_db.keys())
+
+  with open(osp.join(PREPROC_DIR,'imnames.cp'), 'rb') as f:
+    filtered_imnames = set(cp.load(f))
 
   # get the names of the image files in the dataset:
-  imnames = sorted(db['image'].keys())
-  N = len(imnames)
+  N = len(filtered_imnames)
   global NUM_IMG
   if NUM_IMG < 0:
     NUM_IMG = N
   start_idx,end_idx = 0,min(NUM_IMG, N)
 
   RV3 = RendererV3(DATA_PATH,max_time=SECS_PER_IMG)
-  for i in xrange(start_idx,end_idx):
+  for i in range(start_idx,end_idx):
+    if imnames[i] not in filtered_imnames:
+        continue
     imname = imnames[i]
+
     try:
       # get the image:
-      img = Image.fromarray(db['image'][imname][:])
-      # get the pre-computed depth:
-      #  there are 2 estimates of depth (represented as 2 "channels")
-      #  here we are using the second one (in some cases it might be
-      #  useful to use the other one):
-      depth = db['depth'][imname][:].T
-      depth = depth[:,:,1]
-      # get segmentation:
-      seg = db['seg'][imname][:].astype('float32')
-      area = db['seg'][imname].attrs['area']
-      label = db['seg'][imname].attrs['label']
+      img = Image.open(osp.join(PREPROC_DIR, im_dir, imname)).convert('RGB')
+#      img = skimage.io.imread(osp.join(PREPROC_DIR, im_dir, imname))
+
+      depth = depth_db[imname][:].T
+      depth = depth[:,:,0]
+
+      # get segmentation info:
+      seg = seg_db['mask'][imname][:].astype('float32')
+      area = seg_db['mask'][imname].attrs['area']
+      label = seg_db['mask'][imname].attrs['label']
 
       # re-size uniformly:
       sz = depth.shape[:2][::-1]
       img = np.array(img.resize(sz,Image.ANTIALIAS))
       seg = np.array(Image.fromarray(seg).resize(sz,Image.NEAREST))
 
-      print colorize(Color.RED,'%d of %d'%(i,end_idx-1), bold=True)
+      print(colorize(Color.RED,'%d of %d'%(i,end_idx-1), bold=True))
       res = RV3.render_text(img,depth,seg,area,label,
                             ninstance=INSTANCE_PER_IMAGE,viz=viz)
       if len(res) > 0:
@@ -122,14 +137,16 @@ def main(viz=False):
         add_res_to_db(imname,res,out_db)
       # visualize the output:
       if viz:
-        if 'q' in raw_input(colorize(Color.RED,'continue? (enter to continue, q to exit): ',True)):
+        if 'q' in input(colorize(Color.RED,'continue? (enter to continue, q to exit): ',True)):
           break
     except:
       traceback.print_exc()
-      print colorize(Color.GREEN,'>>>> CONTINUING....', bold=True)
+      print(colorize(Color.GREEN,'>>>> CONTINUING....', bold=True))
       continue
-  db.close()
+#  db.close()
   out_db.close()
+  depth_db.close()
+  seg_db.close()
 
 
 if __name__=='__main__':
